967d1fb995d15808ca7cd7d3aa9807e4d5635296
ZevVladimir <zvladimi@zvladimipc>
2023-03-07 07:35:12 -0500

    finding radii of particles to halo works. still issue withdensity calculation

--- assign_particles_to_halos.py
+++ assign_particles_to_halos.py
@@ -1,17 +1,26 @@
 import numpy as np
 import time
 from pygadgetreader import *
+from scipy.spatial import cKDTree
+from scipy import constants
+from colossus.cosmology import cosmology
+import matplotlib.pyplot as plt
 
-file = "/home/zeevvladimir/ML_orbit_infall_project/np_arrays/"
-save_location = "/home/zeevvladimir/ML_orbit_infall_project/np_arrays/"
-snapshot_path = "/home/zeevvladimir/ML_orbit_infall_project/Original_Data/snapshot_190/snapshot_0190"
+
+file = "/home/zvladimi/ML_orbit_infall_project/np_arrays/"
+save_location =  "/home/zvladimi/ML_orbit_infall_project/np_arrays/"
+snapshot_path = "/home/zvladimi/ML_orbit_infall_project/particle_data/snapshot_190/snapshot_0190"
 
 snapshot = 190
 red_shift = readheader(snapshot_path, 'redshift')
 scale_factor = 1/(1+red_shift)
+cosmol = cosmology.setCosmology("bolshoi")
+rho_c = cosmol.rho_c(red_shift)
+
 
 box_size = readheader(snapshot_path, 'boxsize') #units Mpc/h comoving
 box_size = box_size * 10**3 * scale_factor #convert to Kpc/h physical
+print("box_size", box_size)
 
 #load particle info
 particles_pid = np.load(file + "particle_pid.npy")
@@ -19,182 +28,136 @@ particles_vel = np.load(file + "particle_vel.npy")
 particles_pos = np.load(file + "particle_pos.npy") 
 particles_pos = particles_pos * 10**3 * scale_factor
 particles_mass = np.load(file + "all_particle_mass.npy")
-mass = particles_mass[0] * 10**10 #units M_sun/h
+mass = particles_mass[0] * 10e10 #units M_sun/h
 
 #load halo info
-halo_position = np.load(file + "halo_position.npy")
-halo_position = halo_position[:,snapshot,:] * 10**3 * scale_factor
-halo_velocity = np.load(file + "halo_velocity.npy")
-halo_velocity = halo_velocity[:,snapshot,:]
-halo_last_snap = np.load(file + "halo_last_snap.npy")
-halo_r200 = np.load(file + "halo_R200m.npy")
-halo_r200 = halo_r200[:,snapshot]
+halos_pos = np.load(file + "halo_position.npy")
+halos_pos = halos_pos[:,snapshot,:] * 10**3 *scale_factor
+halos_vel = np.load(file + "halo_velocity.npy")
+halos_vel = halos_vel[:,snapshot,:]
+halos_last_snap = np.load(file + "halo_last_snap.npy")
+halos_r200 = np.load(file + "halo_R200m.npy")
+halos_r200 = halos_r200[:,snapshot]
 
 
 num_particles = particles_pid.size
-num_halos = halo_last_snap.size
+num_halos = halos_last_snap.size
 
+# remove all halos for any halo that doesn't exist beyond snapshot 190
+indices_keep = np.zeros((num_halos))
+indices_keep = np.where(halos_last_snap >= snapshot)
+halos_pos = halos_pos[indices_keep]
+halos_vel = halos_vel[indices_keep]
+halos_r200 = halos_r200[indices_keep]
+new_num_halos = halos_r200.size
+        
+# print("halo pos shape", halos_pos.shape)
+# print("halo x", np.max(halos_pos[:,0]))
+# print("particle pos shape", particles_pos.shape)
+# print("particle x", np.max(particles_pos[:,0]))
 
-#particle_with_halo:
-#CONTAINS: PID, particle_pos_x, particle_pos_y, particle_pos_z, total mass to this point, particle_vel_x, particle_vel_y, particle_vel_z,
-#halo_pos_x, halo_pos_y, halo_pos_z, halo_vel_x, halo_vel_y, halo_vel_z, halo_r200
-#SIZE: (256^3,15)
-particle_with_halo = np.zeros((num_particles,15))
+particle_tree = cKDTree(data = particles_pos, leafsize = 16, balanced_tree = False)
+print("trees built")
 
 #calculate distance of particle from halo
-def calculate_distance(halo_x, halo_y, halo_z, particle_x, particle_y, particle_z):
+def calculate_distance(halo_x, halo_y, halo_z, particle_x, particle_y, particle_z, new_particles):
     x_dist = particle_x - halo_x
+    #print(x_dist)
     y_dist = particle_y - halo_y
+    #print(y_dist)
     z_dist = particle_z - halo_z
+    #print(z_dist)
     half_box_size = box_size/2
-    #make this a np.where? instead want to get indices not values idk...
-    x_within_plus = np.zeros(x_dist.size)
-    x_within_minus = np.zeros(x_dist.size)
-    x_within = np.zeros(x_dist.size)
-    x_within = x_dist[x_dist < half_box_size]
-    x_within_plus = x_dist[(x_dist + box_size) < half_box_size]
-    x_within_minus = x_dist[(x_dist + box_size) < -half_box_size]
     
-    x_pos = np.zeros((x_within.size + x_within_plus.size + x_within_minus.size))
-    x_pos[:x_within.size] = x_within
-    x_pos[x_within.size : x_within.size + x_within_plus.size] = x_within_plus
-    x_pos[x_within.size + x_within_plus.size: x_within.size + x_within_plus.size + x_within_minus.size] = x_within_minus
-
-    y_within_plus = np.zeros(x_dist.size)
-    y_within_minus = np.zeros(x_dist.size)
-    y_within = np.zeros(x_dist.size)
-    y_within = y_dist[y_dist < half_box_size]
-    y_within_plus = y_dist[(y_dist + box_size) < half_box_size]
-    y_within_minus = y_dist[(y_dist + box_size) < -half_box_size]
+    x_within_plus = np.where((x_dist + box_size) < half_box_size)
+    x_within_minus = np.where((x_dist + box_size) < -half_box_size)
     
-    y_pos = np.zeros((y_within.size + y_within_plus.size + y_within_minus.size))
-    y_pos[:y_within.size] = y_within
-    y_pos[y_within.size : y_within.size + y_within_plus.size] = y_within_plus
-    y_pos[y_within.size + y_within_plus.size: y_within.size + y_within_plus.size + y_within_minus.size] = y_within_minus
-
-    z_within_plus = np.zeros(x_dist.size)
-    z_within_minus = np.zeros(x_dist.size)
-    z_within = np.zeros(x_dist.size)
-    z_within_plus = z_dist[(z_dist + box_size) < half_box_size]
-    z_within_minus = z_dist[(z_dist + box_size) < -half_box_size]
-    z_within = z_dist[z_dist < half_box_size]
+    particle_x[x_within_plus] = particle_x[x_within_plus] + box_size
+    particle_x[x_within_minus] = particle_x[x_within_minus] - box_size
     
-    z_pos = np.zeros((z_within.size + z_within_plus.size + z_within_minus.size))
-    z_pos[:z_within.size] = z_within
-    z_pos[z_within.size : z_within.size + z_within_plus.size] = z_within_plus
-    z_pos[z_within.size + z_within_plus.size: z_within.size + z_within_plus.size + z_within_minus.size] = z_within_minus
+    y_within_plus = np.where((y_dist + box_size) < half_box_size)
+    y_within_minus = np.where((y_dist + box_size) < -half_box_size)
+    
+    particle_y[y_within_plus] = particle_y[y_within_plus] + box_size
+    particle_y[y_within_minus] = particle_y[y_within_minus] - box_size
+    
+    z_within_plus = np.where((z_dist + box_size) < half_box_size)
+    z_within_minus = np.where((z_dist + box_size) < -half_box_size)
+    
+    particle_z[z_within_plus] = particle_z[z_within_plus] + box_size
+    particle_z[z_within_minus] = particle_z[z_within_minus] - box_size
 
-
-    x_y_satis = np.intersect1d(x_pos, y_pos)
-    print(x_y_satis)
-    all_satis = np.intersect1d(x_y_satis, z_pos)
-    print(all_satis)
-    #Conditions to check if there are particles in the wrong place due to periodic boundary conditions
-    # for i in range(particle_x.size):
-    # #if the distance (x,y,z) goes over the boundary to the left add the box_size
-    # #if the distance (x,y,z) goes over the boundary to the right subtract the box_size
-    #     if halo_x - particle_x[i] > box_size/2:
-    #         particle_x[i] = particle_x[i] + box_size
-    #     elif halo_x - particle_x[i] < -box_size/2:
-    #         particle_x[i] = particle_x[i] - box_size
-        
-    #     if halo_y - particle_y[i] > box_size/2:
-    #         particle_y[i] = particle_y[i] + box_size
-    #     elif halo_y - particle_y[i] < -box_size/2:
-    #         particle_y[i] = particle_y[i] - box_size
-
-    #     if halo_z - particle_z[i] > box_size/2:
-    #         particle_z[i] = particle_z[i] + box_size
-    #     elif halo_z - particle_z[i] < -box_size/2:
-    #         particle_z[i] = particle_z[i] - box_size
-
-    #calculate the actual distance
-    distance = np.zeros((num_particles,1))
+    distance = np.zeros((new_particles,1))
     distance = np.sqrt(np.square((halo_x - particle_x)) + np.square((halo_y - particle_y)) + np.square((halo_z - particle_z)))
     
-    return distance 
+    return distance
+    
+def calculate_density(masses, radius):
+    volume = (4/3) * np.pi * np.power(radius,3)
+    print("volume ", volume)
+    return masses/volume
 
+def check_where_r200(my_density):
+    print("200* rho critical: ", 200 * rho_c)
+    return np.where(my_density < (200 * rho_c))
 
-start_particle = 0
-final_particle = 0
+calculated_r200 = np.zeros(halos_r200.size)
 t1 = time.time()
-for halo in range(num_halos):
-    if(halo_last_snap[halo] >= 190):
-        distance_from_halo = calculate_distance(halo_position[halo,0], halo_position[halo,1],halo_position[halo,2], 
-                                                particles_pos[:,0], particles_pos[:,1], particles_pos[:,2])
-        mask_within_r200 = np.where(distance_from_halo <= halo_r200[halo])
-        print(mask_within_r200[0].size)
-        print(np.sort(distance_from_halo))
-        print(halo_r200[halo])
-        
-        new_particles = mask_within_r200[0].size
-        final_particle = final_particle + new_particles
+count = 1
+#for i in range(len(indices)):
+for i in range(1):
+    #find the indices of the particles within the radius
+    indices = particle_tree.query_ball_point(halos_pos[i,:], r = 1.3 * halos_r200[i])
+    
+    print(halos_r200[i])
+    #new particles being added
+    num_new_particles = len(indices)
 
-        use_particle_pid = np.zeros(new_particles)
-        use_particle_pos = np.zeros((new_particles,3))
-        use_particle_vel = np.zeros((new_particles,3))
+    calculated_densities = np.zeros(num_new_particles)
+    print(num_new_particles, " new particles")
+    new_particles = indices
+    
+    #for how many new particles create an array of how much mass there should be
+    use_mass = np.arange(1, num_new_particles + 1, 1) * mass
+    
+    #Only take the positions that where found with the tree
+    current_particles_pos = particles_pos[indices,:]
+    
+    current_halos_pos = halos_pos[i,:]
+    
+    radius = calculate_distance(current_halos_pos[0], current_halos_pos[1], current_halos_pos[2], current_particles_pos[:,0],
+                                current_particles_pos[:,1], current_particles_pos[:,2], num_new_particles)
+    
+    graph1, (plot1) = plt.subplots(1, 1)
+    plot1 = plt.scatter(current_particles_pos[:,0], current_particles_pos[:,1], color = 'r')
+    plot1 = plt.scatter(current_halos_pos[0], current_halos_pos[1], color = 'b')
+    #plt.xlim([0,box_size])
+    #plt.ylim([0,box_size])
+    #plt.show()
+    
+    
+    radius = np.sort(radius)
+    print("radius: ",radius)
+    #print("masses: ", use_mass)
+    
+    calculated_densities = calculate_density(use_mass, radius)
+    print("densities: ", calculated_densities)
 
-        use_particle_pid = particles_pid[mask_within_r200]
-        use_particle_pos = particles_pos[mask_within_r200]
-        use_particle_vel = particles_vel[mask_within_r200]
-        use_mass = np.arange(1, new_particles+1, 1) * mass
-
-        use_halo_pos = halo_position[halo]
-        use_halo_vel = halo_velocity[halo]
-        use_halo_r200 = halo_r200[halo]
-
-
-        particle_with_halo[start_particle:final_particle, 0] = use_particle_pid
-        particle_with_halo[start_particle:final_particle, 1] = use_particle_pos[:,0]
-        particle_with_halo[start_particle:final_particle, 2] = use_particle_pos[:,1]
-        particle_with_halo[start_particle:final_particle, 3] = use_particle_pos[:,2]
-        particle_with_halo[start_particle:final_particle, 4] = use_mass
-        particle_with_halo[start_particle:final_particle, 5] = use_particle_vel[:,0]
-        particle_with_halo[start_particle:final_particle, 6] = use_particle_vel[:,1]
-        particle_with_halo[start_particle:final_particle, 7] = use_particle_vel[:,2]
-        particle_with_halo[start_particle:final_particle, 8] = use_halo_pos[0]
-        particle_with_halo[start_particle:final_particle, 9] = use_halo_pos[1]
-        particle_with_halo[start_particle:final_particle, 10] = use_halo_pos[2]
-        particle_with_halo[start_particle:final_particle, 11] = use_halo_vel[0]
-        particle_with_halo[start_particle:final_particle, 12] = use_halo_vel[1]
-        particle_with_halo[start_particle:final_particle, 13] = use_halo_vel[2]
-        particle_with_halo[start_particle:final_particle, 14] = use_halo_r200
-
-
-        start_particle = final_particle
-    if (halo % 5000 == 0 and halo != 0):
-        print(halo)
+    
+    indices_r200_met = check_where_r200(calculated_densities)
+    print("Where calculated density exceeds: ", indices_r200_met[:5])
+    
+    if indices_r200_met[0].size == 1:
+        calculated_r200[i] = radius[indices_r200_met[0][0]]
+    elif indices_r200_met[0].size == 0:
+        calculated_r200[i] = 0
+    else:
+        calculated_r200[i] = (radius[indices_r200_met[0][0]] + radius[indices_r200_met[0][1]])/2
+    
+    if (i % 2500 == 0 and i != 0):
         t2 = time.time()
-        print(t2 - t1)
-
-start_particle = 0
-final_particle = 0
-count = 0
-difference = 0
-#loop through halos 
-
-# for halo in range(halo_n.size):
-#     final_particle += halo_n[halo]
-#     #if it is a halo we want
-#     if halo_last_snap[halo] >= snapshot:
-#         difference = final_particle - start_particle
-#         for particle in range(difference): 
-#             particle_with_halo[count][0] = particles_pid[start_particle + particle] #making sure to skip any particles that belong to other halos
-#             particle_with_halo[count][1] = halo_position[halo][snapshot][0]
-#             particle_with_halo[count][2] = halo_position[halo][snapshot][1]
-#             particle_with_halo[count][3] = halo_position[halo][snapshot][2]
-#             particle_with_halo[count][4] = halo_velocity[halo][snapshot][0] 
-#             particle_with_halo[count][5] = halo_velocity[halo][snapshot][1]
-#             particle_with_halo[count][6] = halo_velocity[halo][snapshot][2]
-#             particle_with_halo[count][7] = halo_n[halo]
-#             particle_with_halo[count][8] = halo_r200[halo][snapshot]
-#             count += 1
-#     start_particle = final_particle
-#     if (halo % 5000 == 0 and halo != 0):
-#         print(halo)
-#         t2 = time.time()
-#         print(t2 - t1)
-
-
-print(particle_with_halo)
-np.save(save_location + "particle_with_halo", particle_with_halo)
+        print(i, (t2 - t1))
+        print("remaining: ", (len(indices) - (count * 2500)))
+    
+print("calculated R200: ", calculated_r200)
+print("Actual R200: ", halos_r200)
--- load_save_data.py
+++ load_save_data.py
@@ -5,10 +5,10 @@ import math
 from pygadgetreader import *
 
 
-original_data_file_location = "/home/zeevvladimir/ML_orbit_infall_project/Original_Data/sparta_cbol_l0063_n0256_strd1_v2.hdf5"
-save_location = "/home/zeevvladimir/ML_orbit_infall_project/np_arrays/"
+original_data_file_location = "/home/zvladimi/ML_orbit_infall_project/SPARTA_data/sparta_cbol_l0063_n0256_strd1_v2.hdf5"
+save_location = "/home/zvladimi/ML_orbit_infall_project/np_arrays/"
 
-snapshot_path = "/home/zeevvladimir/ML_orbit_infall_project/Original_Data/snapshot_190/snapshot_0190"
+snapshot_path = "/home/zvladimi/ML_orbit_infall_project/particle_data/snapshot_190/snapshot_0190"
 
 array = np.empty(0)
 
@@ -29,27 +29,27 @@ original_data_file = h5py.File(original_data_file_location)
 
 
 with h5py.File(original_data_file_location, "r") as f: 
-    # halo_last_snap = np.empty(f["/halos/last_snap"].shape, f["/halos/last_snap"].dtype)
-    # halo_last_snap = np.array(list(f["/halos/last_snap"]))
-    # np.save(save_location + "halo_last_snap", halo_last_snap)
+    halo_last_snap = np.empty(f["/halos/last_snap"].shape, f["/halos/last_snap"].dtype)
+    halo_last_snap = np.array(list(f["/halos/last_snap"]))
+    np.save(save_location + "halo_last_snap", halo_last_snap)
 
-#     #save halo R200, velocity, position, halo_id as .np files
-#     halo_R200m = np.empty(f["/halos/R200m"].shape, f["/halos/R200m"].dtype)
-#     halo_R200m = np.array(list(f["/halos/R200m"]))
-#     np.save(save_location + "halo_R200m", halo_R200m)
+    #save halo R200, velocity, position, halo_id as .np files
+    halo_R200m = np.empty(f["/halos/R200m"].shape, f["/halos/R200m"].dtype)
+    halo_R200m = np.array(list(f["/halos/R200m"]))
+    np.save(save_location + "halo_R200m", halo_R200m)
 
-#     halo_velocity = np.empty(f["/halos/velocity"].shape, f["/halos/velocity"].dtype)
-#     halo_velocity = np.array(list(f["/halos/velocity"]))
-#     np.save(save_location + "halo_velocity", halo_velocity)
+    halo_velocity = np.empty(f["/halos/velocity"].shape, f["/halos/velocity"].dtype)
+    halo_velocity = np.array(list(f["/halos/velocity"]))
+    np.save(save_location + "halo_velocity", halo_velocity)
 
-#     
-#     halo_position = np.empty(f["/halos/position"].shape, f["/halos/position"].dtype)
-#     halo_position = np.array(list(f["/halos/position"]))
-#     np.save(save_location + "halo_position", halo_position)
+    
+    halo_position = np.empty(f["/halos/position"].shape, f["/halos/position"].dtype)
+    halo_position = np.array(list(f["/halos/position"]))
+    np.save(save_location + "halo_position", halo_position)
 
-    # halo_id = np.empty(f["/halos/id"].shape, f["/halos/id"].dtype)
-    # halo_id = np.array(list(f["/halos/id"]))
-    # np.save(save_location + "halo_id", halo_id)
+    halo_id = np.empty(f["/halos/id"].shape, f["/halos/id"].dtype)
+    halo_id = np.array(list(f["/halos/id"]))
+    np.save(save_location + "halo_id", halo_id)
 
 # save info from res_ifl
 
@@ -65,17 +65,17 @@ with h5py.File(original_data_file_location, "r") as f:
 
 #save info from all snapshots
 
-# particles_pos = readsnap(snapshot_path, 'pos', 'dm')
-# np.save(save_location + "particle_pos", particles_pos)
+particles_pos = readsnap(snapshot_path, 'pos', 'dm')
+np.save(save_location + "particle_pos", particles_pos)
 
-# particles_vel = readsnap(snapshot_path, 'vel', 'dm')
-# np.save(save_location + "particle_vel", particles_vel)
+particles_vel = readsnap(snapshot_path, 'vel', 'dm')
+np.save(save_location + "particle_vel", particles_vel)
 
-# particles_pid = readsnap(snapshot_path, 'pid', 'dm')
-# np.save(save_location + "particle_pid", particles_pid)
+particles_pid = readsnap(snapshot_path, 'pid', 'dm')
+np.save(save_location + "particle_pid", particles_pid)
 
-# particle_mass = readsnap(snapshot_path, 'mass', 'dm')
-# np.save(save_location + "all_particle_mass", particle_mass)
+particle_mass = readsnap(snapshot_path, 'mass', 'dm')
+np.save(save_location + "all_particle_mass", particle_mass)
 
 
 
