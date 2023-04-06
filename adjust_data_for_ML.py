fix periodic bndry conds, graph average rad vel vs pos

adjust_data_for_ML.py
@@ -61,6 +61,11 @@ def calculate_distance(halo_x, halo_y, halo_z, particle_x, particle_y, particle_
     x_dist = particle_x - halo_x
     y_dist = particle_y - halo_y
     z_dist = particle_z - halo_z
+    
+    coord_diff = np.zeros((new_particles, 3))
+    coord_diff[:,0] = x_dist
+    coord_diff[:,1] = y_dist
+    coord_diff[:,2] = z_dist
 
     half_box_size = box_size/2
     
@@ -68,28 +73,37 @@ def calculate_distance(halo_x, halo_y, halo_z, particle_x, particle_y, particle_
     #be within half a box size of the halo
     #do this for x, y, and z coords
     x_within_plus = np.where((x_dist + box_size) < half_box_size)
-    x_within_minus = np.where((x_dist + box_size) < -half_box_size)
+    x_within_minus = np.where((x_dist - box_size) > -half_box_size)
     
     particle_x[x_within_plus] = particle_x[x_within_plus] + box_size
     particle_x[x_within_minus] = particle_x[x_within_minus] - box_size
     
+    coord_diff[x_within_plus,0] = x_dist[x_within_plus] + box_size
+    coord_diff[x_within_minus,0] = x_dist[x_within_minus] - box_size
+    
     y_within_plus = np.where((y_dist + box_size) < half_box_size)
-    y_within_minus = np.where((y_dist + box_size) < -half_box_size)
+    y_within_minus = np.where((y_dist - box_size) > -half_box_size)
     
     particle_y[y_within_plus] = particle_y[y_within_plus] + box_size
     particle_y[y_within_minus] = particle_y[y_within_minus] - box_size
     
+    coord_diff[y_within_plus,0] = y_dist[y_within_plus] + box_size
+    coord_diff[y_within_minus,0] = y_dist[y_within_minus] - box_size
+    
     z_within_plus = np.where((z_dist + box_size) < half_box_size)
-    z_within_minus = np.where((z_dist + box_size) < -half_box_size)
+    z_within_minus = np.where((z_dist - box_size) > -half_box_size)
     
     particle_z[z_within_plus] = particle_z[z_within_plus] + box_size
     particle_z[z_within_minus] = particle_z[z_within_minus] - box_size
+    
+    coord_diff[z_within_plus,0] = z_dist[z_within_plus] + box_size
+    coord_diff[z_within_minus,0] = z_dist[z_within_minus] - box_size
 
     #calculate distance with standard sqrt((x_1-x_2)^2 + (y_1 - y_2)^2 + (z_1 - z_2)^2)
     distance = np.zeros((new_particles,1))
     distance = np.sqrt(np.square((halo_x - particle_x)) + np.square((halo_y - particle_y)) + np.square((halo_z - particle_z)))
     
-    return distance
+    return distance, coord_diff
 
 #calculates density within sphere of given radius with given mass and calculating volume at each particle's radius
 def calculate_density(masses, radius):
@@ -110,14 +124,19 @@ def brute_force(curr_particles_pos, r200, halo_x, halo_y, halo_z):
     return within_box[np.where(brute_radii <= r200)]
 
 calculated_r200 = np.zeros(halos_r200.size)
+# particle_halo_assign: pid, halo_id, radius, x_dist, y_dist, z_dist, indices
+particle_halo_assign = np.zeros((particles_pid.size,7))
+particles_per_halo = np.zeros(num_halos)
+start = 0
 
 for i in range(num_halos):
     #find the indices of the particles within the expected r200 radius multiplied by 1.4 
     #value of 1.4 determined by guessing if just r200 value or 1.1 miss a couple halo r200 values but 1.4 gets them all
-    indices = particle_tree.query_ball_point(halos_pos[i,:], r = 1.4 * halos_r200[i])
+    indices = particle_tree.query_ball_point(halos_pos[i,:], r = 1.1 * halos_r200[i])
 
     # how many new particles being added
     num_new_particles = len(indices)
+    particles_per_halo[i] = num_new_particles
 
     # sort the particles
     new_particles = np.sort(indices)
@@ -127,16 +146,31 @@ for i in range(num_halos):
 
     #Only take the particle positions that where found with the tree
     current_particles_pos = particles_pos[indices,:]
+    current_particles_pid = particles_pid[indices]
     current_halos_pos = halos_pos[i,:]
     
+    #assign particles to their corresponding halos
+    particle_halo_assign[start:start+num_new_particles,0] = current_particles_pid
+    particle_halo_assign[start:start+num_new_particles,1] = i
+        
     #calculate the radii of each particle based on the distance formula
-    radius = calculate_distance(current_halos_pos[0], current_halos_pos[1], current_halos_pos[2], current_particles_pos[:,0],
+    radius, coord_dist = calculate_distance(current_halos_pos[0], current_halos_pos[1], current_halos_pos[2], current_particles_pos[:,0],
                                 current_particles_pos[:,1], current_particles_pos[:,2], num_new_particles)
 
     #sort the radii and positions to allow for creation of plots and to correctly assign how much mass there is
     arrsortrad = radius.argsort()
     radius = radius[arrsortrad[::1]]
     current_particles_pos = current_particles_pos[arrsortrad[::1]]
+    coord_dist = coord_dist[arrsortrad[::1]]
+    
+    particle_halo_assign[start:start+num_new_particles,2] = radius
+    particle_halo_assign[start:start+num_new_particles,3] = coord_dist[:,0]
+    particle_halo_assign[start:start+num_new_particles,4] = coord_dist[:,1]
+    particle_halo_assign[start:start+num_new_particles,5] = coord_dist[:,2]
+    particle_halo_assign[start:start+num_new_particles,6] = np.array(indices, dtype = int)
+    #print(particle_halo_assign[start:start+num_new_particles,6])
+    
+    
     
     #calculate the density at each particle
     calculated_densities = np.zeros(num_new_particles)
@@ -169,10 +203,125 @@ for i in range(num_halos):
     #if multiple indices choose the first two and average them
     else:
         calculated_r200[i] = (radius[indices_r200_met[0][0]] + radius[indices_r200_met[0][1]])/2
+    start += num_new_particles
     
 difference_r200 = halos_r200 - calculated_r200
-print(np.where(calculated_r200 == 0))
-print("largest difference: ", np.max(difference_r200))
-print("smallest difference: ", np.min(difference_r200))
-print("median difference: ", np.median(difference_r200))
-print("average difference: ", np.average(difference_r200))
+
+print("finish position")
+
+# remove rows with zeros
+particle_halo_assign = particle_halo_assign[~np.all(particle_halo_assign == 0, axis=1)]
+# how many particles are we workin with
+num_particles_identified = particle_halo_assign.shape[0]
+# where are the separations between halos
+indices_change = np.where(particle_halo_assign[:-1,1] != particle_halo_assign[1:,1])[0]  
+
+# take indices from search and use to get velocities
+use_particle_vel = np.zeros((num_particles_identified,3))
+particle_indices = particle_halo_assign[:,6].astype(int)
+use_particle_vel = particles_vel[particle_indices,:]
+
+root_a = np.sqrt(scale_factor)
+use_particle_vel = use_particle_vel * root_a
+
+particles_vel_pec = np.zeros((num_particles_identified,3))
+start = 0
+
+# calculate peculiar velocity by subtracting halo velocity from particle velocity
+for i in range(indices_change.size):
+    finish = start + indices_change[i]
+    particles_vel_pec[start:finish,:] = use_particle_vel[start:finish,:] - halos_vel[i,:]  
+    start = finish
+
+def calc_rhat(radius, x_dist, y_dist, z_dist, num_part):
+    # radius is the magnitude (distance formula)
+    rhat = np.zeros((num_part,3))
+
+    # get unit vector by dividing components by magnitude
+    rhat[:,0] = x_dist/radius
+    rhat[:,1] = y_dist/radius
+    rhat[:,2] = z_dist/radius
+
+    return rhat
+
+
+all_rhat = np.zeros((particles_vel_pec.shape[0],3))
+particles_per_halo = particles_per_halo.astype(int)
+particles_vel_phys = np.zeros((particles_vel_pec.shape[0],3))
+particles_vel_tan = np.zeros((particles_vel_pec.shape[0],3))
+
+
+# convert peculiar velocity to physical velocity
+for i in range(particles_vel_pec.shape[0]):
+    particles_vel_phys[i,:] = particles_vel_pec[i,:] + scale_factor * h * particle_halo_assign[i,2]
+ 
+# calculate tangent velocity which is v_physical * the unit vector for that particle/halo
+start = 0   
+for i in range(particles_per_halo.size):
+    finish = start + particles_per_halo[i]
+
+    all_rhat[start:finish,:] = calc_rhat(particle_halo_assign[start:finish,2], particle_halo_assign[start:finish,3], particle_halo_assign[start:finish,4],
+                         particle_halo_assign[start:finish,5], particles_per_halo[i])
+    
+    particles_vel_tan[start:finish,:] = particles_vel_phys[start:finish,:] * all_rhat[start:finish,:]
+    
+    start = finish
+
+# calculate radial velocity by pythagorean theorem
+particles_vel_rad = np.sqrt(np.square(particles_vel_phys) - np.square(particles_vel_tan))
+use_radius = particle_halo_assign[:,2]
+
+print(np.min(np.sqrt(np.sum(np.square(particles_vel_rad), axis = 1))))
+print(np.max(np.sqrt(np.sum(np.square(particles_vel_rad), axis = 1))))
+#MAKE SURE TO BIN LOGARITHMICALLY
+def make_bins(num_bins, radius, vel_rad):
+    radius = radius[radius != 0]
+    
+    arrsortrad = radius.argsort()
+    radius = radius[arrsortrad[::1]]
+    vel_rad = vel_rad[arrsortrad[::1]]
+
+    min_dist = radius[0]
+    max_dist = radius[-1]
+    #bin_size = (max_dist - min_dist)/num_bins
+    bins = np.logspace(np.log10(min_dist), np.log10(max_dist), num_bins)
+    #bins = np.logspace(min_dist, max_dist, num_bins)
+    print("min dist: ", min_dist)
+    print("max dist: ", max_dist)
+    start = 0
+    average_val = np.zeros((num_bins,2))
+    for i in range(num_bins - 1):
+        bin_size = bins[i + 1] - bins[i]
+        finish = start + bin_size
+
+        indices = np.where((radius >= start) & (radius <= finish))
+        if indices[0].size != 0:
+            start_index = indices[0][0]
+            end_index = indices[0][-1]
+            average_val[i,0] = np.mean(radius[start_index:end_index], dtype=np.float64)
+            average_val[i,1] = np.nanmean(np.sqrt(np.sum(np.square(vel_rad[start_index:end_index,:]), axis = 1)),dtype=np.float64)
+        
+        
+        start = finish
+
+    return average_val
+
+num_bins = 100
+avg_vel_rad = make_bins(num_bins, use_radius, particles_vel_rad)
+
+graph1, (plot1, plot2) = plt.subplots(1,2)
+
+plot1.set_title("average radial velocity vs position without exponential")
+plot1.set_xlabel("position")
+plot1.set_ylabel("average rad vel")
+plot1.set_xscale("log")
+
+plot1.plot(avg_vel_rad[:num_bins - 23,0], avg_vel_rad[:num_bins - 23,1], color = 'b')
+
+plot2.set_title("average radial velocity vs position with exponential")
+plot2.set_xlabel("position")
+plot2.set_ylabel("average rad vel")
+plot2.set_xscale("log")
+
+plot2.plot(avg_vel_rad[:,0], avg_vel_rad[:,1], color = 'r')
+plt.show()