f47b742ad85f12970c1c584da0f801b77e041cbf
ZevVladimir <zevvladimir3002@gmail.com>
2023-04-26 23:15:13 -0400

    plotting based on nu bins

--- find_velocities_ML.py
+++ find_velocities_ML.py
@@ -5,7 +5,9 @@ from colossus.cosmology import cosmology
 import matplotlib.pyplot as plt
 from colossus.halo import mass_so
 from colossus.utils import constants
+from colossus.lss import peaks
 import time
+from matplotlib.pyplot import cm
 
 G = constants.G
 
@@ -39,7 +41,7 @@ halos_id = halos_id[:,snapshot]
 halos_status = np.load(save_location + "halo_status.npy")
 halos_status = halos_status[:,snapshot]
 
-indices_keep = np.zeros((halos_id.size))
+indices_keep = np.zeros((halos_id.size),dtype = np.int32)
 indices_keep = np.where((halos_last_snap >= snapshot) & (halos_status == 10))
 
 halos_vel = halos_vel[indices_keep]
@@ -56,31 +58,18 @@ num_particles_identified = particle_halo_assign_id.shape[0]
 indices_change = np.where(particle_halo_assign_id[:-1,1] != particle_halo_assign_id[1:,1])[0] + 1
 indices_change = np.append(indices_change, particle_halo_assign_id.shape[0]) #add last index
 
-# choose a halo mass bin
-mass_hist = np.histogram(all_halo_mass, 100)
-use_mass_start = 15
-use_mass_finish = use_mass_start + 5
-print("mass range:", mass_hist[1][use_mass_start] * 1e-12, "to", mass_hist[1][use_mass_finish] * 1e-12, "10^12 solar masses")
-print(np.sum(mass_hist[0][use_mass_start:use_mass_finish]))
-# get all the indices where the halo masses are within the selected bounds
-halos_mass_indices = np.where((all_halo_mass > mass_hist[1][use_mass_start]) & (all_halo_mass < mass_hist[1][use_mass_finish]))[0]
-
-# Record the particle incdices that the halo starts at until the next halo starts
-halos_use_indices_finish = indices_change[(halos_mass_indices + 1)]
-halos_use_indices_start = indices_change[(halos_mass_indices)]
-halos_use_indices = np.column_stack((halos_use_indices_start,halos_use_indices_finish))
 
 # take indices from search and use to get velocities
-use_particle_vel = np.zeros((num_particles_identified,3))
+use_particle_vel = np.zeros((num_particles_identified,3), dtype = np.float32)
 particle_indices = particle_halo_assign_id[:,2].astype(int)
 use_particle_vel = particles_vel[particle_indices,:] 
 root_a = np.sqrt(scale_factor)
 use_particle_vel = use_particle_vel * root_a
 
-particles_vel_pec = np.zeros((num_particles_identified,3))
+particles_vel_pec = np.zeros((num_particles_identified,3), dtype = np.float32)
 
 # calculate peculiar velocity by subtracting halo velocity from particle velocity
-radius_div_r200 = np.zeros((particle_distances.size))
+radius_div_r200 = np.zeros((particle_distances.size), dtype = np.float32)
 start_vel_pec = 0
 for i in range(indices_change.size):
     finish_vel_pec = indices_change[i]
@@ -90,7 +79,7 @@ for i in range(indices_change.size):
     start_vel_pec = finish_vel_pec
 
 def calc_rhat(x_dist, y_dist, z_dist):
-    rhat = np.zeros((x_dist.size,3))
+    rhat = np.zeros((x_dist.size,3), dtype = np.float32)
     # get unit vector by dividing components by magnitude
     magnitude = np.sqrt(np.square(x_dist) + np.square(y_dist) + np.square(z_dist))
     rhat[:,0] = x_dist/magnitude
@@ -115,6 +104,7 @@ all_rhat = calc_rhat(particle_halo_radius_comp[:,1], particle_halo_radius_comp[:
 
 # convert peculiar velocity to physical velocity by adding scale factor * h * dist from particle to halo
 start_vel_phys = 0
+
 # loop through each halo's particles
 for i in range(indices_change.size):
     finish_vel_phys = indices_change[i]
@@ -146,26 +136,23 @@ for i in range(indices_change.size):
     
     start_vel_phys = finish_vel_phys
     
-def make_bins(num_bins, radius, radius_r200, vel_rad):
-    # remove the blank parts of the radius
-    radius = radius[radius != 0]
-    radius_r200 = radius_r200[radius_r200 != 0]
-    
+def make_bins(num_bins, radius, radius_r200, vel_rad):    
     min_rad = np.min(radius_r200)
     max_rad = np.max(radius_r200)
     bins = np.logspace(np.log10(min_rad), np.log10(max_rad), num_bins)
 
     bin_start = 0
-    average_val_part = np.zeros((num_bins,2))
-    average_val_hubble = np.zeros((num_bins,2))
+    average_val_part = np.zeros((num_bins,2), dtype = np.float32)
+    average_val_hubble = np.zeros((num_bins,2), dtype = np.float32)
     
     for i in range(num_bins - 1):
         bin_size = bins[i + 1] - bins[i]
         bin_finish = bin_start + bin_size
         # make sure there are points within the bins
         indices = np.where((radius_r200 >= bin_start) & (radius_r200 <= bin_finish))
-
+    
         if indices[0].size != 0:
+            
             use_vel_rad = vel_rad[indices[0]]
             average_val_part[i,0] = np.average(np.array([bin_start,bin_finish]))
             average_val_part[i,1] = np.average(use_vel_rad) 
@@ -184,41 +171,72 @@ def make_bins(num_bins, radius, radius_r200, vel_rad):
     return average_val_part, average_val_hubble
 
 print("start binning")
-num_bins = 50
-mass_bin_radius = np.zeros(particle_distances.size)
-mass_bin_radius_div_r200 = np.zeros(particle_distances.size)
-mass_bin_vel_rad = np.zeros(particles_vel_rad.size)
+peak_heights = peaks.peakHeight(all_halo_mass, red_shift,)
+mass_hist = np.histogram(peak_heights, 50)
 
-start_mass_bin = 0
-finish_mass_bin = 0
-for i in range(halos_use_indices.shape[0]):
-    finish_mass_bin += (halos_use_indices[i][1] - halos_use_indices[i][0])
-    mass_bin_radius[start_mass_bin:finish_mass_bin] = particle_distances[halos_use_indices[i][0]:halos_use_indices[i][1]]
-    mass_bin_radius_div_r200[start_mass_bin:finish_mass_bin] = radius_div_r200[halos_use_indices[i][0]:halos_use_indices[i][1]]
-    mass_bin_vel_rad[start_mass_bin:finish_mass_bin] = particles_vel_rad[halos_use_indices[i][0]:halos_use_indices[i][1]]
-    start_mass_bin = finish_mass_bin
+def split_by_mass(start_nu, finish_nu, num_bins):
+    
+    # get all the indices where the halo masses are within the selected bounds
+    halos_mass_indices = np.where((peak_heights > start_nu) & (peak_heights < finish_nu))[0]
+    print(halos_mass_indices.shape)
+    # Record the particle indices that the halo starts at until the next halo starts
+    halos_use_indices_start = indices_change[(halos_mass_indices)]
+    halos_use_indices_finish = indices_change[(halos_mass_indices + 1)]
+    halos_use_indices = np.column_stack((halos_use_indices_start,halos_use_indices_finish))
+
+    total_part_use = np.sum((halos_use_indices_finish - halos_use_indices_start))
+    mass_bin_radius = np.zeros(total_part_use, dtype = np.float32)
+    mass_bin_radius_div_r200 = np.zeros(total_part_use, dtype = np.float32)
+    mass_bin_vel_rad = np.zeros(total_part_use, dtype = np.float32)
+    
+    start_mass_bin = 0
+    finish_mass_bin = 0
+    for i in range(halos_use_indices.shape[0]):
+        finish_mass_bin += (halos_use_indices[i][1] - halos_use_indices[i][0])
+        
+        mass_bin_radius[start_mass_bin:finish_mass_bin] = particle_distances[halos_use_indices[i][0]:halos_use_indices[i][1]]
+        mass_bin_radius_div_r200[start_mass_bin:finish_mass_bin] = radius_div_r200[halos_use_indices[i][0]:halos_use_indices[i][1]]
+        mass_bin_vel_rad[start_mass_bin:finish_mass_bin] = particles_vel_rad[halos_use_indices[i][0]:halos_use_indices[i][1]]
+        
+        start_mass_bin = finish_mass_bin
+        
+    # mass_bin_radius = mass_bin_radius[mass_bin_radius != 0]
+    # mass_bin_radius_div_r200 = mass_bin_radius_div_r200[mass_bin_radius_div_r200 != 0]
+    # mass_bin_vel_rad = mass_bin_vel_rad[mass_bin_vel_rad != 0]
+    
+    avg_vel_rad_part, avg_vel_rad_hub = make_bins(num_bins, mass_bin_radius, mass_bin_radius_div_r200, mass_bin_vel_rad)
+    avg_vel_rad_part = avg_vel_rad_part[~np.all(avg_vel_rad_part == 0, axis=1)]
+    avg_vel_rad_hub = avg_vel_rad_hub[~np.all(avg_vel_rad_hub == 0, axis=1)]
+    
+    arr1inds = avg_vel_rad_hub[:,0].argsort()
+    avg_vel_rad_hub[:,0] = avg_vel_rad_hub[arr1inds[:],0]
+    avg_vel_rad_hub[:,1] = avg_vel_rad_hub[arr1inds[:],1]
+    
+    return avg_vel_rad_part, avg_vel_rad_hub
+
+# start and finish track where we are in the array
+plt.rcParams['text.usetex'] = True
+num_bins = 50
+start_nu = 1
+num_iter = 5
+color = iter(cm.rainbow(np.linspace(0, 1, num_iter)))
+for i in range(num_iter):
+    c = next(color)
+    finish_nu = start_nu + .5
+    avg_vel_rad_part, avg_vel_rad_hub = split_by_mass(start_nu, finish_nu, num_bins)
+    
+    plt.plot(avg_vel_rad_part[:,0], avg_vel_rad_part[:,1], color = c, label = r"${0} < \nu < {1}$".format(str(start_nu), str(finish_nu)))
+    start_nu += .5
     
 
-# mass_bin_radius = mass_bin_radius[mass_bin_radius != 0]
-avg_vel_rad_part, avg_vel_rad_hub = make_bins(num_bins, mass_bin_radius, mass_bin_radius_div_r200, mass_bin_vel_rad)
-#avg_vel_rad_part, avg_vel_rad_hub = make_bins(num_bins, particle_distances, radius_div_r200, particles_vel_rad)
-
-avg_vel_rad_part = avg_vel_rad_part[~np.all(avg_vel_rad_part == 0, axis=1)]
-avg_vel_rad_hub = avg_vel_rad_hub[~np.all(avg_vel_rad_hub == 0, axis=1)]
-
-arr1inds = avg_vel_rad_hub[:,0].argsort()
-avg_vel_rad_hub[:,0] = avg_vel_rad_hub[arr1inds[:],0]
-avg_vel_rad_hub[:,1] = avg_vel_rad_hub[arr1inds[:],1]
-
-graph1, (plot1) = plt.subplots(1,1)
-plot1.plot(avg_vel_rad_hub[:,0], avg_vel_rad_hub[:,1], color = "purple", label = "hubble flow")
-plot1.plot(avg_vel_rad_part[:,0], avg_vel_rad_part[:,1], color = "blue", label = "particles")
-plot1.set_title("average radial velocity vs position all particles")
-plot1.set_xlabel("position $r/R_{200m}$")
-plot1.set_ylabel("average rad vel $v_r/v_{200m}$")
-plot1.set_xscale("log")    
-
-plot1.legend()
+plt.plot(avg_vel_rad_hub[:,0], avg_vel_rad_hub[:,1], color = "purple", label = "hubble flow")
+plt.title("average radial velocity vs position all particles")
+plt.xlabel("position $r/R_{200m}$")
+plt.ylabel("average rad vel $v_r/v_{200m}$")
+plt.xscale("log")    
+plt.ylim([-.5,.5])
+plt.xlim([0.01,10])
+plt.legend()
 t2 = time.time()
 print("velocity finished: ", (t2 - t1)," seconds")
 
--- load_save_data.py
+++ load_save_data.py
@@ -28,55 +28,55 @@ original_data_file = h5py.File(original_data_file_location)
 # print(allkeys(original_data_file))
 
 
-# with h5py.File(original_data_file_location, "r") as f: 
-#     halo_last_snap = np.empty(f["/halos/last_snap"].shape, f["/halos/last_snap"].dtype)
-#     halo_last_snap = np.array(list(f["/halos/last_snap"]))
-#     np.save(save_location + "halo_last_snap", halo_last_snap)
+with h5py.File(original_data_file_location, "r") as f: 
+    halo_last_snap = np.empty(f["/halos/last_snap"].shape, f["/halos/last_snap"].dtype)
+    halo_last_snap = np.array(list(f["/halos/last_snap"]))
+    np.save(save_location + "halo_last_snap", halo_last_snap)
 
-#     #save halo R200, velocity, position, halo_id as .np files
-#     halo_R200m = np.empty(f["/halos/R200m"].shape, f["/halos/R200m"].dtype)
-#     halo_R200m = np.array(list(f["/halos/R200m"]))
-#     np.save(save_location + "halo_R200m", halo_R200m)
+    #save halo R200, velocity, position, halo_id as .np files
+    halo_R200m = np.empty(f["/halos/R200m"].shape, f["/halos/R200m"].dtype)
+    halo_R200m = np.array(list(f["/halos/R200m"]))
+    np.save(save_location + "halo_R200m", halo_R200m)
 
-#     halo_velocity = np.empty(f["/halos/velocity"].shape, f["/halos/velocity"].dtype)
-#     halo_velocity = np.array(list(f["/halos/velocity"]))
-#     np.save(save_location + "halo_velocity", halo_velocity)
+    halo_velocity = np.empty(f["/halos/velocity"].shape, f["/halos/velocity"].dtype)
+    halo_velocity = np.array(list(f["/halos/velocity"]))
+    np.save(save_location + "halo_velocity", halo_velocity)
 
     
-#     halo_position = np.empty(f["/halos/position"].shape, f["/halos/position"].dtype)
-#     halo_position = np.array(list(f["/halos/position"]))
-#     np.save(save_location + "halo_position", halo_position)
+    halo_position = np.empty(f["/halos/position"].shape, f["/halos/position"].dtype)
+    halo_position = np.array(list(f["/halos/position"]))
+    np.save(save_location + "halo_position", halo_position)
 
-#     halo_id = np.empty(f["/halos/id"].shape, f["/halos/id"].dtype)
-#     halo_id = np.array(list(f["/halos/id"]))
-#     np.save(save_location + "halo_id", halo_id)
-    # halo_status = np.empty(f["/halos/status"].shape, f["/halos/status"].dtype)
-    # halo_status = np.array(list(f["/halos/status"]))
-    # np.save(save_location + "halo_status", halo_status)
-# # save info from res_ifl
+    halo_id = np.empty(f["/halos/id"].shape, f["/halos/id"].dtype)
+    halo_id = np.array(list(f["/halos/id"]))
+    np.save(save_location + "halo_id", halo_id)
+    halo_status = np.empty(f["/halos/status"].shape, f["/halos/status"].dtype)
+    halo_status = np.array(list(f["/halos/status"]))
+    np.save(save_location + "halo_status", halo_status)
+# save info from res_ifl
 
-#     halo_first = np.empty(f["/tcr_ptl/res_ifl/halo_first"].shape, f["/tcr_ptl/res_ifl/halo_first"].dtype)
-#     halo_first = np.array(list(f["/tcr_ptl/res_ifl/halo_first"]))
-#     np.save(save_location + "halo_first", halo_first)
+    halo_first = np.empty(f["/tcr_ptl/res_ifl/halo_first"].shape, f["/tcr_ptl/res_ifl/halo_first"].dtype)
+    halo_first = np.array(list(f["/tcr_ptl/res_ifl/halo_first"]))
+    np.save(save_location + "halo_first", halo_first)
 
-#     halo_n = np.empty(f["/tcr_ptl/res_ifl/halo_n"].shape, f["/tcr_ptl/res_ifl/halo_n"].dtype)
-#     halo_n = np.array(list(f["/tcr_ptl/res_ifl/halo_n"]))
-#     np.save(save_location + "halo_n", halo_n)
+    halo_n = np.empty(f["/tcr_ptl/res_ifl/halo_n"].shape, f["/tcr_ptl/res_ifl/halo_n"].dtype)
+    halo_n = np.array(list(f["/tcr_ptl/res_ifl/halo_n"]))
+    np.save(save_location + "halo_n", halo_n)
     
 
 
 
 #save info from all snapshots
 
-# particles_pos = readsnap(snapshot_path, 'pos', 'dm')
-# np.save(save_location + "particle_pos_192", particles_pos)
+particles_pos = readsnap(snapshot_path, 'pos', 'dm')
+np.save(save_location + "particle_pos_192", particles_pos)
 
-# particles_vel = readsnap(snapshot_path, 'vel', 'dm')
-# np.save(save_location + "particle_vel_192", particles_vel)
+particles_vel = readsnap(snapshot_path, 'vel', 'dm')
+np.save(save_location + "particle_vel_192", particles_vel)
 
-# particles_pid = readsnap(snapshot_path, 'pid', 'dm')
-# np.save(save_location + "particle_pid_192", particles_pid)
+particles_pid = readsnap(snapshot_path, 'pid', 'dm')
+np.save(save_location + "particle_pid_192", particles_pid)
 
-# particle_mass = readsnap(snapshot_path, 'mass', 'dm')
-# np.save(save_location + "all_particle_mass_192", particle_mass)
+particle_mass = readsnap(snapshot_path, 'mass', 'dm')
+np.save(save_location + "all_particle_mass_192", particle_mass)
 
