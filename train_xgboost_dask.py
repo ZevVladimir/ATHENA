actually fixed rotation in rv-tv and greatly sped up process by computing dask array to np for plotting

train_xgboost_dask.py
@@ -186,30 +186,32 @@ if __name__ == "__main__":
     del dtrain
     del dtest
 
-    with open(train_dataset_loc, "rb") as file:
-        X = pickle.load(file)
-    with open(train_labels_loc, "rb") as file:
-        y = pickle.load(file)
-    X = da.from_array(X,chunks=(chunk_size,X.shape[1]))
+    # with open(train_dataset_loc, "rb") as file:
+    #     X = pickle.load(file)
+    # with open(train_labels_loc, "rb") as file:
+    #     y = pickle.load(file)
+    # X = da.from_array(X,chunks=(chunk_size,X.shape[1]))
     
-    train_prediction = dxgb.inplace_predict(client, bst, X)
-    train_prediction = np.round(train_prediction)
+    # train_prediction = dxgb.inplace_predict(client, bst, X).compute()
+    # train_prediction = np.round(train_prediction)
 
     # print("Train Report")
     #print(classification_report(y, train_prediction))
 
-    del X
-    del y
+    # del X
+    # del y
 
+    t1 = time.time()
     with open(test_dataset_loc, "rb") as file:
         X_np = pickle.load(file)
     with open(test_labels_loc, "rb") as file:
         y_np = pickle.load(file)
     X = da.from_array(X_np,chunks=(chunk_size,X_np.shape[1]))
     
-    test_prediction = dxgb.inplace_predict(client, bst, X)
+    test_prediction = dxgb.inplace_predict(client, bst, X).compute()
     test_prediction = np.round(test_prediction)
-    
+    t2 = time.time()
+    print("Predictions finished:", np.round((t2-t1),2),"sec", np.round(((t2-t1)/60),2), "min")
     # print("Test Report")
     # print(classification_report(y_np, test_prediction))
 

visualization_functions.py
@@ -191,17 +191,17 @@ def create_hist_max_ptl(min_ptl, inf_radius, orb_radius, inf_rad_vel, orb_rad_ve
     if bin_r_vr == None:
         orb_r_rv = np.histogram2d(orb_radius, orb_rad_vel, bins=num_bins, range=[[0,max_radius],[min_rad_vel,max_rad_vel]])
         orb_r_tv = np.histogram2d(orb_radius, orb_tang_vel, bins=num_bins, range=[[0,max_radius],[min_tang_vel,max_tang_vel]])
-        orb_rv_tv = np.histogram2d(orb_tang_vel, orb_rad_vel, bins=num_bins, range=[[min_tang_vel,max_tang_vel],[min_rad_vel,max_rad_vel]])
+        orb_rv_tv = np.histogram2d(orb_rad_vel, orb_tang_vel, bins=num_bins, range=[[min_rad_vel,max_rad_vel],[min_tang_vel,max_tang_vel]])
         inf_r_rv = np.histogram2d(inf_radius, inf_rad_vel, bins=[orb_r_rv[1],orb_r_rv[2]], range=[[0,max_radius],[min_rad_vel,max_rad_vel]])
         inf_r_tv = np.histogram2d(inf_radius, inf_tang_vel, bins=[orb_r_tv[1],orb_r_tv[2]], range=[[0,max_radius],[min_tang_vel,max_tang_vel]])
-        inf_rv_tv = np.histogram2d(inf_rad_vel, inf_tang_vel, bins=[orb_rv_tv[1],orb_rv_tv[2]], range=[[min_tang_vel,max_tang_vel],[min_rad_vel,max_rad_vel]])
+        inf_rv_tv = np.histogram2d(inf_rad_vel, inf_tang_vel, bins=[orb_rv_tv[1],orb_rv_tv[2]], range=[[min_rad_vel,max_rad_vel],[min_tang_vel,max_tang_vel]])
     else:
         orb_r_rv = np.histogram2d(orb_radius, orb_rad_vel, bins=bin_r_vr, range=[[0,max_radius],[min_rad_vel,max_rad_vel]])
         orb_r_tv = np.histogram2d(orb_radius, orb_tang_vel, bins=bin_r_vt, range=[[0,max_radius],[min_tang_vel,max_tang_vel]])
-        orb_rv_tv = np.histogram2d(orb_tang_vel, orb_rad_vel, bins=bin_vr_vt, range=[[min_tang_vel,max_tang_vel],[min_rad_vel,max_rad_vel]])
+        orb_rv_tv = np.histogram2d(orb_rad_vel, orb_tang_vel, bins=bin_vr_vt, range=[[min_rad_vel,max_rad_vel],[min_tang_vel,max_tang_vel]])
         inf_r_rv = np.histogram2d(inf_radius, inf_rad_vel, bins=bin_r_vr, range=[[0,max_radius],[min_rad_vel,max_rad_vel]])
         inf_r_tv = np.histogram2d(inf_radius, inf_tang_vel, bins=bin_r_vt, range=[[0,max_radius],[min_tang_vel,max_tang_vel]])
-        inf_rv_tv = np.histogram2d(inf_rad_vel, inf_tang_vel, bins=bin_vr_vt, range=[[min_tang_vel,max_tang_vel],[min_rad_vel,max_rad_vel]])
+        inf_rv_tv = np.histogram2d(inf_rad_vel, inf_tang_vel, bins=bin_vr_vt, range=[[min_rad_vel,max_rad_vel],[min_tang_vel,max_tang_vel]])
         
     orb_r_rv[0][orb_r_rv[0] < min_ptl] = min_ptl
     orb_r_tv[0][orb_r_tv[0] < min_ptl] = min_ptl
@@ -226,9 +226,6 @@ def plot_incorrectly_classified(correct_labels, ml_labels, radii, rad_vel, tang_
     min_tang_vel = np.min(tang_vel)
        
     incorrect_orbit = np.where((ml_labels == 1) & (correct_labels == 0))[0]
-    print(incorrect_orbit)
-    print(ml_labels[:10])
-    print(correct_labels[:10])
     print("num incorrect orb", incorrect_orbit.shape)
     incorrect_infall = np.where((ml_labels == 0) & (correct_labels == 1))[0]
     print("num incorrect inf", incorrect_infall.shape)