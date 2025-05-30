7100bf16be0afc4ac3451712751cd5ca6dbd543d
ZevVladimir <zevvladimir3002@gmail.com>
2025-05-30 13:56:09 -0400

    compare ML to fast and SPARTA to fast separately in orbiting frac

--- src/orb_frac_by_rad.py
+++ src/orb_frac_by_rad.py
@@ -8,8 +8,9 @@ mpl.rcParams.update(mpl.rcParamsDefault)
 import dask.dataframe as dd
 from dask import delayed
 import pandas as pd
+import xgboost as xgb
 
-from utils.ML_support import setup_client, get_combined_name, parse_ranges, load_sparta_mass_prf, load_data, extract_snaps, get_feature_labels, set_cosmology, filter_ddf, filter_df, split_sparta_hdf5_name
+from utils.ML_support import setup_client, get_combined_name, make_preds, parse_ranges, get_model_name, load_data, extract_snaps, get_feature_labels, set_cosmology, filter_ddf, filter_df, split_sparta_hdf5_name
 from utils.data_and_loading_functions import create_directory, load_pickle, load_config, load_pickle, timed, load_SPARTA_data
 from src.utils.ke_cut_support import fast_ke_predictor, opt_ke_predictor
 from src.utils.vis_fxns import plt_SPARTA_KE_dist
@@ -23,6 +24,9 @@ debug_plt_path = config_params["PATHS"]["debug_plt_path"]
 
 pickle_data = config_params["MISC"]["pickle_data"]
 
+use_gpu = config_params["DASK_CLIENT"]["use_gpu"]
+
+model_sims = config_params["TRAIN_MODEL"]["model_sims"]
 features = config_params["TRAIN_MODEL"]["features"]
 target_column = config_params["TRAIN_MODEL"]["target_column"]
 
@@ -63,6 +67,12 @@ if __name__ == "__main__":
     plot_loc = opt_model_fldr_loc + dset_name + "_" + test_comb_name + "/plots/"
     create_directory(plot_loc)
     
+    comb_model_sims = get_combined_name(model_sims) 
+        
+    model_name = get_model_name(model_type, model_sims)    
+    model_fldr_loc = path_to_models + comb_model_sims + "/" + model_type + "/"
+    model_save_loc = model_fldr_loc + model_name + ".json"
+    
     dset_params = load_pickle(ML_dset_path + curr_test_sims[0] + "/dset_params.pickle")
     sim_cosmol = dset_params["cosmology"]
     all_tdyn_steps = dset_params["t_dyn_steps"]
@@ -70,6 +80,15 @@ if __name__ == "__main__":
     feature_columns = get_feature_labels(features,all_tdyn_steps)
     snap_list = extract_snaps(curr_test_sims[0])
     cosmol = set_cosmology(sim_cosmol)
+    
+    try:
+        bst = xgb.Booster()
+        bst.load_model(model_save_loc)
+        if use_gpu:
+            bst.set_param({"device": "cuda:0"})
+        print("Loaded Model Trained on:",model_sims)
+    except:
+        print("Couldn't load Booster Located at: " + model_save_loc)
     
     if os.path.isfile(opt_model_fldr_loc + "ke_optparams_dict.pickle"):
         print("Loading parameters from saved file")
@@ -116,6 +135,10 @@ if __name__ == "__main__":
         
         preds_fit_ke = opt_ke_predictor(opt_param_dict, bins, r_test.compute().to_numpy(), vr_test.compute().to_numpy(), lnv2_test.compute().to_numpy(), r_cut_calib)
         
+        preds_ML = make_preds(client, bst, X_df)
+        print(preds_ML)
+        
+        
         y_df = y_df.values.compute().squeeze()
         X_df = X_df.compute()
         
@@ -126,6 +149,8 @@ if __name__ == "__main__":
 
         # Step 2: Initialize counters
         num_bins = len(bins) - 1
+        sparta_orbiting_counts = np.zeros(num_bins)
+        sparta_infalling_counts = np.zeros(num_bins)
         ml_orbiting_counts = np.zeros(num_bins)
         ml_infalling_counts = np.zeros(num_bins)
         ke_orbiting_counts = np.zeros(num_bins)
@@ -133,12 +158,15 @@ if __name__ == "__main__":
     
         for i in range(num_bins):
             in_bin = bin_indices == i
-            ml_orbiting_counts[i] = np.sum(y_df[in_bin] == 1)
-            ml_infalling_counts[i] = np.sum(y_df[in_bin] == 0)
+            sparta_orbiting_counts[i] = np.sum(y_df[in_bin] == 1)
+            sparta_infalling_counts[i] = np.sum(y_df[in_bin] == 0)
+            ml_orbiting_counts[i] = np.sum(preds_ML[in_bin] == 1)
+            ml_infalling_counts[i] = np.sum(preds_ML[in_bin] == 0)
             ke_orbiting_counts[i] = np.sum(preds_fast_ke[in_bin] == 1)
             ke_infalling_counts[i] = np.sum(preds_fast_ke[in_bin] == 0)
 
         with np.errstate(divide='ignore', invalid='ignore'):
+            sparta_ratio = np.where(sparta_infalling_counts > 0, sparta_orbiting_counts / (sparta_infalling_counts + sparta_orbiting_counts), np.nan)
             ml_ratio = np.where(ml_infalling_counts > 0, ml_orbiting_counts / (ml_infalling_counts + ml_orbiting_counts), np.nan)
             ke_ratio = np.where(ke_infalling_counts > 0, ke_orbiting_counts / (ke_infalling_counts + ke_orbiting_counts), np.nan)
         bin_centers = np.sqrt(bins[:-1] * bins[1:])
@@ -154,5 +182,15 @@ if __name__ == "__main__":
         ax.set_xlabel(r"Radius $r/R_{\rm 200m}$")
         ax.set_ylabel(r"$N_{\rm orb}/N_{\rm tot}$")
         
-        fig.savefig(debug_plt_path + test_comb_name + "_orb_rat_by_rad.png")
+        fig.savefig(debug_plt_path + test_comb_name + "_ML_fast_orb_rat_by_rad.png")
+        
+        fig, ax = plt.subplots(1, figsize=(10,5))
+        ax.bar(bin_centers - offset, sparta_ratio, width=bar_widths, label='SPARTA Classification', alpha=0.7)
+        ax.bar(bin_centers + offset, ke_ratio, width=bar_widths, label='Fast KE Cut Classification', alpha=0.7)     
+        ax.set_xscale("log")  
+        ax.legend()
+        ax.set_xlabel(r"Radius $r/R_{\rm 200m}$")
+        ax.set_ylabel(r"$N_{\rm orb}/N_{\rm tot}$")
+        
+        fig.savefig(debug_plt_path + test_comb_name + "_sparta_fast_orb_rat_by_rad.png")
         
